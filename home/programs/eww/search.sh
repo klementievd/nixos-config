a6e772002d46643f97b2c552b6a7012f44117188
Klementiev Dmitry <klementievd@proton.me>
2023-06-01 22:16:33 +0500

    Eww bar changes

--- home/programs/eww/search.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-
-list=$(ls /usr/bin/ | grep -m 10 -i "$1")
-buf=""
-for l in $list ; do
-  buf="$buf (button :class \"item\" :onclick \"$l &\" \"$l\")"
-done
-echo "(box :orientation \"v\" :spacing 5 :class \"apps\" :halign \"center\" :valign \"center\" $buf)" > $XDG_CONFIG_HOME/eww/search_items.txt
