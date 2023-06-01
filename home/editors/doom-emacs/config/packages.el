84e54dd242a26ccd28e4c9be5e5e04f9bdedcb21
Klementiev Dmitry <klementievd@proton.me>
2023-06-01 18:11:22 +0500

    Trying to create ms-windows'like taskspanel

--- home/editors/doom-emacs/config/packages.el
+++ home/editors/doom-emacs/config/packages.el
@@ -48,3 +48,10 @@
 ;(unpin! pinned-package another-pinned-package)
 ;; ...Or *all* packages (NOT RECOMMENDED; will likely break things)
 ;(unpin! t)
+
+;;; Code:
+
+(package! yuck-mode)
+(add-to-list 'auto-mode-alist '("\\.yuck\\'" . yuck-mode))
+
+;;; packages.el ends here
