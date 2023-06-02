a0d794f9e3457862b044535701a1662ebfff8a4e
Klementiev Dmitry <klementievd@proton.me>
2023-06-02 17:37:37 +0500

    Minor changes

---
+++ home/programs/eww/scripts/search.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+list=$(ls ~/.nix-profile/bin | grep -m 10 -i "$1")
+buf=""
+for l in $list ; do
+  buf="$buf (button :class \"finded-item\" :onclick \"$l &\" \"$l\")"
+done
+echo "(box :orientation \"v\" :spacing 5 :class \"apps\" :halign \"center\" :valign \"center\" $buf)" > ~/.config/eww/scripts/search_items.txt
