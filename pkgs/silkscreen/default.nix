061218a340de71fe4cc3dd8b78eac63f13799dd3
Klementiev Dmitry <klementievd@proton.me>
2023-06-04 17:43:13 +0500

    Minor changes

--- pkgs/silkscreen/default.nix
+++ pkgs/silkscreen/default.nix
@@ -7,12 +7,4 @@ in fetchzip {
   url = "https://media.fontsgeek.com/download/zip/s/i/silkscreen-regular_0JTIE.zip";
 
   sha256 = "sha256-KpKC+SGwf/TuQe3gCzTw7UE7rSHVosW5Fru9ehvs5Lw=";
-
-  meta = with lib; {
-    homepage = "https://rsms.me/inter/";
-    description = "A typeface specially designed for user interfaces";
-    license = licenses.ofl;
-    platforms = platforms.all;
-    maintainers = with maintainers; [ demize dtzWill ];
-  };
 }
